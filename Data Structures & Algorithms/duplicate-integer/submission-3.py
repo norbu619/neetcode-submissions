d993b7d79b7e505559262d1f0cd31947f61c6b21
norbu law <62362283+norbu619@users.noreply.github.com>
2026-09-07 10:59:52 +1200

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        my_dict = {}
+
+        for i in nums:
+            if i in my_dict:
+                return True
+            my_dict[i] = "hey"
+        return False
